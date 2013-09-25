9fbf0f8c5859f1e86393cd6fbd918631867502d2
Josue Mora <josue@inno-live.com>
2013-09-25 02:09:55 -0700

    New class members for printing all data separetely

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <map>
 using namespace std;
 
@@ -38,34 +39,57 @@ class RBAC{
 				g.open("PRA.txt", ios::in);
 				if(!g){ cout << "PRA.txt - file not found "; exit(1); }
 				
-				for(int i = 0; i < 6; ++i){
-					g >> y[i].role;
-					g >> y[i].action;
-					g >> y[i].object;
+				for(int i = 0; i < 3; ++i){
+						g >> y[i].role;
+						g >> y[i].action;
+						g >> y[i].object;
 				}
 
 		}
-		void userLogin(){
+		void userLogin()
+		{
+			string u1;
+			
 			cout << "Login: ";
 			cin >> username;
-			for(int i = 0; i < 3; ++i){
-				if( (x[i].name) == username ){
+			
+			for(int i = 0; i < 3; ++i)
+			{
+				u1 = x[i].name;
+				if( ( u1.compare(username) == 0 ))
+				{
 					cout << "Welcome " << username << "!\n"; 
 				}
-				else{
+				else
+				{
 					cout << "Error user " << username << " is not in the database\n";
 					break;
 				}
 			}
 		}
+		
 		void printUserRoles(){
+			cout << "Roles: " <<"\n";
 			for(int i =0; i < 3; ++i){
-				cout << x[i].name << "\t" << x[i].role << "\n";
+				cout << x[i].role << "\n";
+			}
+		}
+		void printUserNames(){
+			cout << "Usernames: " <<"\n";
+			for(int i = 0; i < 3; ++i){
+				cout << x[i].name << "\n";
 			}
 		}
 		void printActions(){
+			cout << "Actions: " <<"\n";
 			for(int i =0; i < 3; ++i){
-				cout << y[i].role << "\t" << y[i].action << "\t" << y[i].object << "\n";
+				cout << y[i].action << "\n";
+			}
+		}
+		void printObjects(){
+			cout << "Objects: " <<"\n";
+			for(int i =0; i < 3; ++i){
+				cout << y[i].object << "\t";
 			}
 		}
 };
@@ -73,10 +97,10 @@ class RBAC{
 int main(void){
 	RBAC A;
 	A.loadFiles();
-	A.userLogin();
-	//A.printUserRoles();
-	//A.printActions();
-	//A.printRoles();
-	
+	//A.userLogin();
+	A.printUserRoles();
+	A.printActions();
+	A.printUserRoles();
+	A.printUserNames();
 	return(0);
 }
