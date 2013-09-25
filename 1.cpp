c406f802eefeb2cea9ca1a228f7fbe0ae4cbe5cf
Josue Mora <josue@inno-live.com>
2013-09-25 13:54:54 -0700

    Fixed login issue

--- 1.cpp
+++ 1.cpp
@@ -21,11 +21,14 @@ class RBAC{
 		string username;
 		USER x[3];
 		ACTION y[9];
+		int user_id;
+		string command;
+		string user_action;
+		string user_object;
+		bool a;
 		
 	public:
-			RBAC(){
-				
-			}
+			RBAC(){}
 			void loadFiles(){
 				
 				f.open("URA.txt", ios::in);
@@ -46,28 +49,89 @@ class RBAC{
 				}
 
 		}
-		void userLogin()
-		{
-			string u1;
+		bool userLogin(){	
 			
 			cout << "Login: ";
-			cin >> username;
+			getline(cin,username);
+			
+			if(username == "Alberto"){
+				user_id = 1;
+				a = true;
+			}
+			else if(username == "Kevin"){
+				user_id = 2;
+				a = true;
+			}
+			else if(username == "Cesar"){
+				user_id = 3;
+				a = true;
+			}
+			else{
+				cout << "error- username not found";
+				a = false;
+				cout << a;
+			}
 			
 			for(int i = 0; i < 3; ++i)
 			{
-				u1 = x[i].name;
-				if( ( u1.compare(username) == 0 ))
-				{
-					cout << "Welcome " << username << "!\n"; 
-				}
-				else
-				{
-					cout << "Error user " << username << " is not in the database\n";
+				if( username == x[i].name){
+					cout << "Welcome " << username << "!\n";
 					break;
 				}
 			}
+
+			return a;
+			
 		}
-		
+		void getUserAction(){
+			cout << "cmd> ";
+			getline(cin, command);
+			
+			int sp1 = command.find(' ');
+			if (sp1 == -1){
+				cout << "Wrong entry - Please input command followed by object";
+			}
+			else{
+				int sp2 = command.find(' ', sp1+1);
+				if(sp2==-1){
+					user_action = command.substr(0, sp1);
+					user_object = command.substr(sp1+1, command.length()-sp1);
+				}
+			}
+		}
+		void verifyUserPermissions()
+		{
+			
+				if((user_id == 1 )&&(user_action == "levitate"))
+				{
+					cout << "Access granted by virtue of roles: ";
+					cout << y[0].role << endl;
+				}
+				else if((user_id == 1 )&&(user_action != "levitate")){
+					cout << "Access denied: you are not authorized to perform this action!";
+					cout << endl;
+				}
+				else if((user_id == 2 )&&(user_action == "climb"))
+				{
+					cout << "Access granted by virtue of roles: ";
+					cout << y[1].role << endl;
+				}
+				else if((user_id == 2 )&&(user_action != "climb")){
+					cout << "Access denied: you are not authorized to perform this action!";
+					cout << endl;
+				}
+				else if((user_id == 3 )&&(user_action == "abduct"))
+				{
+					cout << "Access granted by virtue of roles: ";
+					cout << y[1].role << endl;
+				}
+				else if((user_id == 3 )&&(user_action != "abduct")){
+					cout << "Access denied: you are not authorized to perform this action!";
+					cout << endl;
+				}
+
+				
+		}
 		void printUserRoles(){
 			cout << "Roles: " <<"\n";
 			for(int i =0; i < 3; ++i){
@@ -92,15 +156,19 @@ class RBAC{
 				cout << y[i].object << "\t";
 			}
 		}
+		~RBAC(){}
 };
 
-int main(void){
+int main(){
 	RBAC A;
+	bool access = true;
+
 	A.loadFiles();
-	//A.userLogin();
-	A.printUserRoles();
-	A.printActions();
-	A.printUserRoles();
-	A.printUserNames();
+	access = A.userLogin();
+	if(access == true){
+		A.getUserAction();
+		A.verifyUserPermissions();
+	}
+
 	return(0);
 }
